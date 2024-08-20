a39734161da9fc03029f53eaa8db8815bdcb6abb
Hwirin-Kim <kimhuirin@gmail.com>
2024-08-20 21:54:29 +0900

    [Silver IV] Title: 동전 0, Time: 96 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -2,18 +2,13 @@ let fs = require('fs')
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let [N, goal] = input[0].split(' ').map(Number);
 const array = input.slice(1).map(Number);
+const sortedArray = array.sort((a,b)=> b - a)
 let count = 0;
 
-
-for(let i = N-1; i>=0; i--){
-    while(array[i]<=goal){
-   goal -= array[i];
-   count++;
-   }
-    
-    if (goal === 0) {
-        break;
+sortedArray.forEach((currentCoin)=>{
+    if(currentCoin<=goal){
+    count = count + Math.floor(goal/currentCoin)
+    goal = goal%currentCoin
     }
-}
-
+})
 console.log(count)
