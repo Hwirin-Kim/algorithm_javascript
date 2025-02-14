925516071dbc469cb97db24649f797e2ba442394
Hwirin-Kim <kimhuirin@gmail.com>
2025-02-14 21:35:07 +0900

    [Silver III] Title: 주유소, Time: 280 ms, Memory: 44028 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,16 +1,17 @@
-const fs = require('fs')
-const input = fs.readFileSync('dev/stdin').toString().split('\n')
-const distanceArray = input[1].split(' ').map(Number)
-const priceArray = input[2].split(' ').map(Number)
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let totalPrice = 0
-let minPrice = priceArray[0]
-for (let i=0; i<distanceArray.length; i++){
-    if(minPrice>priceArray[i]){
-        minPrice = priceArray[i]
+const distanceArray = input[1].trim().split(' ').map(item => BigInt(item));
+const priceArray = input[2].trim().split(' ').map(item => BigInt(item));
+
+let totalPrice = BigInt(0);
+let minPrice = priceArray[0];
+
+for (let i = 0; i < distanceArray.length; i++) {
+    if (minPrice > priceArray[i]) {
+        minPrice = priceArray[i];
     }
-    const price = distanceArray[i] * minPrice
-    totalPrice += price
+    totalPrice += distanceArray[i] * minPrice;
 }
 
-console.log(totalPrice)
+console.log(totalPrice.toString());
